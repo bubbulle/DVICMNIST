357feafc134585f740566eb92e360aee12697405
bubbulle <berenicedufaure25@gmailcom>
2021-03-30 16:30:33 +0200

    correct

--- docs/static/scripts/script.js
+++ docs/static/scripts/script.js
@@ -1,26 +1,27 @@
 let content = document.getElementById("content");
 let digits = [];
-for(let i = 0; i < 10; i++){
-  let digits = {
-    container: null,
-    content: {
-      bar: document.CreateElement("div"),
-      label: document.CreateElement("div"),
+
+for(let i = 0; i < 10; i++) {
+    let digit = {
+        container: document.createElement("div"),
+        content: {
+            bar: document.createElement("div"),
+            label: document.createElement("div"),
+        }
     }
-  }
 
-  digit.container.id = i;
-  digit.content.bar.id = "bar_" + i;
-  digit.content.label.id = "label_" + i;
+    digit.container.id = i;
+    digit.content.bar.id = "bar_" + i;
+    digit.content.label.id = "label_" + i;
 
-  digit.content.bar.addClass("bar");
-  digit.content.label.addClass("label");
+    digit.content.bar.classList.add("bar");
+    digit.content.label.classList.add("label");
 
-  content.appendChild(digit.container);
-  digit.container.appendChild(digit.content.bar);
-  digit.container.appendChild(digit.content.label);
+    content.appendChild(digit.container);
+    digit.container.appendChild(digit.content.bar);
+    digit.container.appendChild(digit.content.label);
 
-  // digit.content
+    digit.content.label.innerHTML = i;
 
-  digit.push(digit);
+    digits.push(digit);
 }
